39f4a2f4de946bfb26710409825ec7a86c97baea
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-14 11:54:20 -0300

    :lock: feat: configurando aplicação para acessar /dashboard somente logado

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from "next";
 import styles from "./dashboard.module.css";
 import Head from "next/head";
+
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
 	return (
@@ -12,3 +15,19 @@ export default function Dashboard() {
 		</div>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	const session = await getSession({ req });
+
+	if (!session?.user) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
+	return {
+		props: {},
+	};
+};
