4acbdaa757af5ef5b60cfa02030b362cb98a8ffa
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-14 22:18:04 -0300

    :construction: interface de tarefa publica criada

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -4,17 +4,39 @@ import { db } from "../../services/firebaseConnection";
 import { doc, collection, query, where, getDoc } from "firebase/firestore";
 
 import styles from "./styles.module.css";
+import { Textarea } from "@/components/Textarea";
 
-export default function Task() {
+interface TaskProps {
+	item: {
+		task: string;
+		created: string;
+		public: boolean;
+		user: string;
+		taskId: string;
+	};
+}
+
+export default function Task({ item }: TaskProps) {
 	return (
-		<>
+		<div className={styles.container}>
 			<Head>
 				<title>Detalhes | Taskify+</title>
 			</Head>
 			<main className={styles.main}>
 				<h1>Task</h1>
+				<article className={styles.task}>
+					<p>{item.task}</p>
+				</article>
 			</main>
-		</>
+
+			<section className={styles.commentsContainer}>
+				<h2>Fazer um comentário</h2>
+				<form>
+					<Textarea placeholder="Digite seu comentário..." />
+					<button className={styles.button}>Enviar comentário</button>
+				</form>
+			</section>
+		</div>
 	);
 }
 
@@ -58,7 +80,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 
 	return {
 		props: {
-			id,
+			item: task,
 		},
 	};
 };
