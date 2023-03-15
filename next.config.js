77e333de0a13625d911e1353860d61c52dbd112c
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-15 13:28:12 -0300

    :bug: fix: corrigindo erro de deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	output: "standalone",
+	compress: false,
+	images: {
+		unoptimized: true,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
